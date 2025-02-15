6a2d4b6efabd899b41a21a4d4cf8063f1404a897
Marco MR <marcomr9641@gmail.com>
2025-02-15 11:20:40 -0600

    configure store with reduxjs toolkit

--- src/reducer.js
+++ src/reducer.js
@@ -1,23 +1,28 @@
+import { configureStore } from '@reduxjs/toolkit';
+
 const initialState = {
   good: 0,
   ok: 0,
   bad: 0
-}
+};
 
 const counterReducer = (state = initialState, action) => {
-  console.log(action)
   switch (action.type) {
     case 'GOOD':
-      return state
+      return { ...state, good: state.good + 1 };
     case 'OK':
-      return state
+      return { ...state, ok: state.ok + 1 };
     case 'BAD':
-      return state
-    case 'ZERO':
-      return state
-    default: return state
+      return { ...state, bad: state.bad + 1 };
+    case 'RESET':
+      return initialState;
+    default:
+      return state;
   }
-  
-}
+};
 
-export default counterReducer
+const store = configureStore({
+  reducer: counterReducer
+});
+
+export { store, counterReducer };
